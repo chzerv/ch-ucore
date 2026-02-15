f25227158058804ae07d6b097f1bbe1a27cac17f
Chris Zervakis <xrist.zerv@protonmail.com>
2026-02-15 17:01:44 +0200

    Switch to ucore:stable-nvidia-lts and install multimedia packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,23 +2,16 @@
 
 set -ouex pipefail
 
-### Install packages
+# Install RPMFusion
+dnf5 install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
+# Install Intel media driver and switch to full ffmpeg
+dnf5 install -y intel-media-driver libva-utils
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y gstreamer1-plugin-libav \
+  gstreamer1-plugins-bad-free-extras \
+  gstreamer1-plugins-bad-freeworld \
+  gstreamer1-plugins-ugly \
+  gstreamer1-vaapi
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
+dnf5 swap -y ffmpeg-free ffmpeg --allowerasing
